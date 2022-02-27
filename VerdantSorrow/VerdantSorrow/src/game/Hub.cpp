a595dccb393d9b70de22e2a85546eace60d2fbc2
Laura <lgomez25@ucm.es>
2022-02-27 17:58:39 +0100

    cambio tamaño del jugador

--- VerdantSorrow/VerdantSorrow/src/game/Hub.cpp
+++ VerdantSorrow/VerdantSorrow/src/game/Hub.cpp
@@ -85,7 +85,7 @@ void Hub::playerGenerator(CollisionManager* colManager, Entity* player_) {
 	auto playerX = sdlutils().width() / 2 - 25;
 	auto playerY = sdlutils().height() / 2 - 25;
 	//Se le dan las posiciones iniciales, vecocidad, ancho y alto al player
-	playerTr->init(Vector2D(playerX, playerY), Vector2D(), 100, 200, 0.0f);
+	playerTr->init(Vector2D(playerX, playerY), Vector2D(), 50, 100, 0.0f);
 	
 	//IMPORTANTE: Ponerlo antes del PlayerCtrl siempre porque si no se salta 2 veces
 	player_->addComponent<CollideWithBorders>();
