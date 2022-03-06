f3f9e10d7ce641d920a874c182ee5ba6ba0acb9a
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2022-03-06 21:06:15 +0100

    animaciones atkFloor/atkAir implementadas

--- VerdantSorrow/VerdantSorrow/src/components/player/Attack.h
+++ VerdantSorrow/VerdantSorrow/src/components/player/Attack.h
@@ -3,6 +3,8 @@
 #include "../../utils/Vector2D.h"
 #include "../RectangleCollider.h"
 #include "../../game/CollisionManager.h"
+#include "../FramedImage.h"
+#include "PlayerAttributes.h"
 #include <SDL.h>
 
 class Transform;
@@ -13,6 +15,7 @@ class Attack : public RectangleCollider
 public:
     __CMPID_DECL__(ecs::_ATTACK)
     Attack(float width, float height, CollisionManager* colManager);
+    //Attack(float width, float height, CollisionManager* colManager, FramedImage* anim);
     ~Attack();
 
     void initComponent() override;
@@ -28,6 +31,8 @@ protected:
     int lastAttack;
 
     CollisionManager* colMan_;
+    FramedImage* anim_;
+    PlayerAttributes* attrib_;
     /**
     * Setea la posicion del ataque delante del jugador, teniendo en cuenta su direccion de movimiento
     */
