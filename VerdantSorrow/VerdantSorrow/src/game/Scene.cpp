d615ccac161d376600cd42d8cb9196822d39cd95
Laura <lgomez25@ucm.es>
2022-03-16 16:20:49 +0100

    creado scene y sceneManager

---
+++ VerdantSorrow/VerdantSorrow/src/game/Scene.cpp
@@ -0,0 +1,25 @@
+#include "Scene.h"
+
+Scene::Scene()
+{
+}
+
+Scene::~Scene()
+{
+}
+
+void Scene::init()
+{
+}
+
+void Scene::start()
+{
+}
+
+void Scene::background()
+{
+}
+
+void Scene::playerGenerator(CollisionManager* colManager, Entity* player_)
+{
+}
