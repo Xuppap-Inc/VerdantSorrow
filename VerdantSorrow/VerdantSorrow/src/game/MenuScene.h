5bac26bd22fe04f2ef6b048b7b93bdfc395e5c74
PaulaMorillasAlonso <pamorill@ucm.es>
2022-03-26 19:19:26 +0100

    botones menu funcionan y salir del juego

--- VerdantSorrow/VerdantSorrow/src/game/MenuScene.h
+++ VerdantSorrow/VerdantSorrow/src/game/MenuScene.h
@@ -3,9 +3,12 @@
 #include <vector>
 #include "Scene.h"
 
-
 class MenuScene : public Scene
 {
+private:
+	//Vector con los nombres de los botones aignados en "menu.json"
+	std::vector<std::string> buttonNames = {  "new_Game","continue", "settings", "controls","quit" };
+	std::vector<Transform *> buttonPositions_;
 public:
 	MenuScene();
 	~MenuScene() {};
@@ -13,9 +16,6 @@ public:
 	void background();
 	void createButton(float x, float y,float w, float  h, std::string buttonImage);
 	void update()override;
-	void onButtonClicked(Vector2D buttonPos);
-private:
-	std::vector<Transform *> buttonPositions;
-	std::vector<Entity*> allButtons;
+	void onButtonClicked(int index);
 };
 
