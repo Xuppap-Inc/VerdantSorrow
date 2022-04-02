84aef179594e7a6cfc534ef5cfc1517bf99408a6
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2022-04-02 11:56:31 +0200

    x

--- VerdantSorrow/VerdantSorrow/src/components/player/Attack.cpp
+++ VerdantSorrow/VerdantSorrow/src/components/player/Attack.cpp
@@ -16,7 +16,7 @@ Attack::Attack(float width, float height, CollisionManager* colManager) :
 
 	// INPUT
 	attackKeys({ SDL_SCANCODE_J }),
-	attackButtons({ SDL_CONTROLLER_BUTTON_B, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER })
+	attackButtons({ SDL_CONTROLLER_BUTTON_X, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER })
 {
 	setActive(false);
 	colMan_ = colManager;
