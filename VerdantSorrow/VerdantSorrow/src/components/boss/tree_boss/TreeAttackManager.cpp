6b13a9ae34f4b0b3c802e41dbd220eeeb90a0c6b
javimuno <javimuno@ucm.es>
2022-04-09 20:09:12 +0200

    Groot ataca en la dirección que debe y tiene Cd en sus ataques

--- VerdantSorrow/VerdantSorrow/src/components/boss/tree_boss/TreeAttackManager.cpp
+++ VerdantSorrow/VerdantSorrow/src/components/boss/tree_boss/TreeAttackManager.cpp
@@ -76,7 +76,8 @@ void TreeAttackManager::update()
 	Vector2D playerPos = player_->getPos();
 	Vector2D treePos = tr_->getPos();
 
-	float distance = playerPos.getX() - treePos.getX();
+	//el arbol es más ancho que su collider, se ajusta jugando con su ancho
+	float distance = playerPos.getX() - treePos.getX()-tr_->getWidth()/7;
 
 	float absDistance = abs(distance);
 
@@ -97,9 +98,11 @@ void TreeAttackManager::update()
 		//si se encuentra a distancia de ataque a melee, ataca
 		if (((dir_<0 &&absDistance < MELEE_ATTACK_DISTANCE) || (dir_ > 0 && absDistance<tr_->getWidth() + MELEE_ATTACK_DISTANCE))  && newAtack_) {
 			std::cout << timerCd_.currTime() << std::endl;
-
+			std::cout << distance << std::endl;
+			std::cout << tr_->getWidth() << std::endl;
 			
 			if(timerCd_.currTime() >= ATTACK_CD) {
+				std::cout << dir_ << std::endl;
 				animNewState_ = ANIM_ATTACK;
 				anim_->repeat(false);
 
