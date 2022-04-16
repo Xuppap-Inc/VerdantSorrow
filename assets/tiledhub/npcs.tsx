96bfc9fefc560f0705df34df373559d3a891ea1c
miriam-m-s <81752837+miriam-m-s@users.noreply.github.com>
2022-04-16 18:09:49 +0200

    npcs xd

---
+++ assets/tiledhub/npcs.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="npcs" tilewidth="1513" tileheight="2386" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="847" height="2009" source="../HUB/npcs/paula/pauladverdad/paula_0001_cuerpo.png"/>
+ </tile>
+ <tile id="1">
+  <image width="1513" height="2386" source="../HUB/npcs/nacho/nacho_0001_nacho.png"/>
+ </tile>
+ <tile id="2">
+  <image width="885" height="2186" source="../HUB/npcs/javi/javideverdad/javi_0001_javi.png"/>
+ </tile>
+</tileset>
