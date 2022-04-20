eae3bc315b75f29bf1659ec8c7875a918942380a
miriam-m-s <81752837+miriam-m-s@users.noreply.github.com>
2022-04-21 01:02:57 +0200

    me muero con tiled

---
+++ VerdantSorrow/VerdantSorrow/resources/Hub/miriam_aqui_va_el_tiled_pls/tileset/arboles1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="arboles1" tilewidth="30" tileheight="30" tilecount="360" columns="24">
+ <image source="../assets/arboles.png" trans="ff00ff" width="730" height="461"/>
+</tileset>
