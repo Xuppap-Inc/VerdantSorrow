d4c6024237321999686a2cc3900bcb89a46c3e7a
damart24 <82331644+damart24@users.noreply.github.com>
2022-04-24 23:00:34 +0200

    Planta generadora de moscas

---
+++ VerdantSorrow/VerdantSorrow/src/components/boss/nivelHuida/FlySpawnerPlant.cpp
@@ -0,0 +1,51 @@
+#include "FlySpawnerPlant.h"
+#include "../../../ecs/Entity.h"
+#include "../../../ecs/manager.h"
+#include "../../Transform.h"
+#include "../wave/WaveMovement.h"
+#include "../frog_boss/TongueAttack.h"
+#include "../finalBoss/ClapAttack.h"
+#include "../../player/PlayerAttributes.h"
+#include "../../FramedImage.h"
+#include "../../tutorial/TutorialFly.h"
+#include "../frog_boss/FlyMovement.h"
+
+
+FlySpawnerPlant::FlySpawnerPlant(CollisionManager* colManager, bool lookingUp, float timer) : tr_(nullptr), colMan_(colManager), lookingUp_(lookingUp), cooldownSpawner_(timer)
+{
+}
+
+FlySpawnerPlant::~FlySpawnerPlant()
+{
+}
+
+void FlySpawnerPlant::initComponent()
+{
+	tr_ = ent_->getComponent<Transform>();
+	vt = mngr_->addTimer();
+	assert(tr_ != nullptr);
+}
+
+void FlySpawnerPlant::update()
+{
+	if (vt->currTime() > cooldownSpawner_) {
+		vt->reset();
+		auto fly = mngr_->addEntity();
+		auto fTr = fly->addComponent<Transform>();
+		float flyY;
+		if (lookingUp_) flyY = tr_->getPos().getY() - 20;
+		else flyY = tr_->getPos().getY() + tr_->getHeight() + 20;
+		auto flyX = tr_->getPos().getX();
+
+		fTr->init(Vector2D(flyX, flyY), Vector2D(), 60, 50, 0.0f);
+		auto col = fly->addComponent<RectangleCollider>(fTr->getWidth(), fTr->getHeight());
+		col->setIsTrigger(true);
+		colMan_->addCollider(col);
+		fly->addComponent<FramedImage>(&sdlutils().images().at("mosca"), 6, 6, (1000 / 30) * 31, 31, "mosca");
+		fly->addComponent<TutorialFly>();
+		fly->addComponent<FlyMovement>();
+		fly->addToGroup(ecs::_BOSSELEMENTS_GRP);
+	}
+}
+
+
---
+++ VerdantSorrow/VerdantSorrow/src/components/boss/nivelHuida/FlySpawnerPlant.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "../../../ecs/Component.h"
+#include "../../../game/CollisionManager.h"
+#include "../../RectangleCollider.h"
+#include "../../../sdlutils/SDLUtils.h"
+
+#include <vector>
+#include <iostream>
+#include <SDL_stdinc.h>
+
+class Transform;
+class FlySpawnerPlant :
+    public ecs::Component
+{
+public:
+    __CMPID_DECL__(ecs::_MUSHROOM)
+        FlySpawnerPlant(CollisionManager* colMan, bool lookingUp, float timer);
+
+    ~FlySpawnerPlant();
+    void initComponent() override;
+    void update() override;
+protected:
+    float cooldownSpawner_;
+    bool lookingUp_;
+    VirtualTimer* vt;
+    Transform* tr_;
+    CollisionManager* colMan_;
+};
