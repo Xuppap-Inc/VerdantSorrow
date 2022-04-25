42bece3461ebb85880ba76da2ba76b83ccaad8e6
miriam-m-s <81752837+miriam-m-s@users.noreply.github.com>
2022-04-25 16:09:57 +0200

    COLLIDERS

--- VerdantSorrow/VerdantSorrow/resources/Hub/tilesets/trees.tsx
+++ VerdantSorrow/VerdantSorrow/resources/Hub/tilesets/trees.tsx
@@ -3,63 +3,33 @@
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="14">
   <image width="239" height="321" source="../assets/arbol.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="77" y="96" width="129" height="150"/>
-  </objectgroup>
  </tile>
  <tile id="15">
   <image width="100" height="132" source="../assets/arbolcementerio.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="31" y="52" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="16">
   <image width="100" height="132" source="../assets/arbolcementerio2.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="33.5" y="45" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="17">
   <image width="100" height="132" source="../assets/arbolcementerio3.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="27.5" y="50" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="18">
   <image width="160" height="215" source="../assets/arbolchiquito.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="62" y="61" width="85" height="112"/>
-  </objectgroup>
  </tile>
  <tile id="19">
   <image width="160" height="215" source="../assets/arbolROJO.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="71" y="60" width="79" height="114"/>
-  </objectgroup>
  </tile>
  <tile id="20">
   <image width="100" height="132" source="../assets/ARBOLROJO3.png"/>
-  <objectgroup draworder="index" id="3">
-   <object id="2" x="29.5" y="55" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="21">
   <image width="100" height="132" source="../assets/ARBOLROJOLIGHT.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="28.5" y="55" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="22">
   <image width="160" height="215" source="../assets/arbolrojox.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="81.5" y="93" width="57" height="77"/>
-  </objectgroup>
  </tile>
  <tile id="23">
   <image width="100" height="209" source="../assets/arbolsinhojas.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="29.5" y="51" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="24">
   <properties>
@@ -69,63 +39,33 @@
  </tile>
  <tile id="25">
   <image width="90" height="90" source="../assets/piedra.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="24.5" y="13" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="26">
   <image width="110" height="110" source="../assets/pino.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="38.5" y="24" width="30" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="27">
   <image width="110" height="110" source="../assets/pino3.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="40.5" y="25" width="29" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="28">
   <image width="100" height="132" source="../assets/rojo.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="29.5" y="39" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="29">
   <image width="100" height="132" source="../assets/arbolmor.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="30.5" y="43" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="30">
   <image width="100" height="132" source="../assets/arbolmorado.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="32.5" y="40" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="31">
   <image width="100" height="132" source="../assets/arbolrosa.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="29.5" y="52" width="41" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="32">
   <image width="110" height="110" source="../assets/pino4.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="2" x="45.5" y="38" width="21" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="33">
   <image width="110" height="110" source="../assets/pinomorado.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="43.5" y="36" width="21" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="34">
   <image width="110" height="110" source="../assets/pinomorado1.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="41.5" y="33" width="21" height="46"/>
-  </objectgroup>
  </tile>
  <tile id="35">
   <image width="1200" height="1200" source="../assets/FUEGO.png"/>
