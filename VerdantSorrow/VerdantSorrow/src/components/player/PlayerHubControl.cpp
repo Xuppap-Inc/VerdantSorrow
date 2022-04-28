e8c8c0433fbf8b248594d3fe520a8affe271eb58
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2022-04-28 22:54:38 +0200

    Arreglado lo de las direcciones de movimiento del hub

--- VerdantSorrow/VerdantSorrow/src/components/player/PlayerHubControl.cpp
+++ VerdantSorrow/VerdantSorrow/src/components/player/PlayerHubControl.cpp
@@ -118,28 +118,31 @@ void PlayerHubControl::handleInput()
 		}
 	}
 
+
+
+	if (moveLeft_)
+		anim_->flipX(true);
+	else
+		anim_->flipX(false);
+
+
 	if (moveDown_) {
 		anim_->changeanim(&sdlutils().imagesHub().at("walk_Kyna"), 3, 9, (1000 / 25) * 25, 25, "walk_Kyna");
-		anim_->flipX(false);
 	}
 
 	else if (moveUp_) {
 		anim_->changeanim(&sdlutils().imagesHub().at("walk_up_Kyna"), 3, 9, (1000 / 25) * 25, 25, "walk_side_Kyna");
-		anim_->flipX(false);
 	}
 
 	else if (moveRight_) {
 		anim_->changeanim(&sdlutils().imagesHub().at("walk_side_Kyna"), 3, 9, (1000 / 25) * 25, 25, "walk_side_Kyna");
-		anim_->flipX(false);
 	}
 
 	else if (moveLeft_) {
 		anim_->changeanim(&sdlutils().imagesHub().at("walk_side_Kyna"), 3, 9, (1000 / 25) * 25, 25, "walk_side_Kyna");
-		anim_->flipX(true);
 	}
 
 	else if (!moveUp_ && !moveDown_ && !moveLeft_ && !moveRight_) {
 		anim_->changeanim(&sdlutils().imagesHub().at("idle_Kyna"), 4, 8, (1000 / 30) * 30, 30, "idle_Kyna");
-		anim_->flipX(false);
 	}
 }
