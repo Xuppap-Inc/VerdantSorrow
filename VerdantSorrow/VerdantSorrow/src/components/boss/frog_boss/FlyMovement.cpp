8ea920d2bbbea978fa1e3f6ffcef8829e334a02b
damart24 <82331644+damart24@users.noreply.github.com>
2022-04-29 11:57:20 +0200

    Mosca del tutorial ya no se volatiliza por arte de magia

--- VerdantSorrow/VerdantSorrow/src/components/boss/frog_boss/FlyMovement.cpp
+++ VerdantSorrow/VerdantSorrow/src/components/boss/frog_boss/FlyMovement.cpp
@@ -26,9 +26,15 @@ void FlyMovement::update()
 	//Calcula velocidad y le da velocidad a la mosca
 	dirX_ = (playerTr_->getPos().getX() - tr_->getPos().getX()) / abs(playerTr_->getPos().getX() - tr_->getPos().getX());
 	dirY_ = (playerTr_->getPos().getY() - tr_->getPos().getY()) / abs(playerTr_->getPos().getY() - tr_->getPos().getY());
-	vel = new Vector2D(dirX_ * SPEED, dirY_ * SPEED);
+	//4 excepciones porque siempre es speed hasta que de repente es -20045096 y se va a parla
+	if (dirY_ < -SPEED) dirY_ = -SPEED;
+	else if (dirY_ > SPEED) dirY_ = SPEED;
+	if (dirX_ < -SPEED) dirX_ = -SPEED;
+	else if (dirX_ > SPEED) dirX_ = SPEED;
+
+	vel = new Vector2D(dirX_ * 1, dirY_ * 1);;
 	
 	//Si queda poco espacio explota y muere
-	if (abs(playerTr_->getPos().getX() - tr_->getPos().getX()) < spaceWhereExplotes || abs(playerTr_->getPos().getY() - tr_->getPos().getY() < spaceWhereExplotes))
-		ent_->setAlive(false);
+	//if (abs(playerTr_->getPos().getX() - tr_->getPos().getX()) < spaceWhereExplotes || abs(playerTr_->getPos().getY() - tr_->getPos().getY() < spaceWhereExplotes))
+	//	ent_->setAlive(false);
 }
