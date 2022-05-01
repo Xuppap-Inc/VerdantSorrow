35f47ed7617bcb61f225ed9d2063b672fd7cba62
Laura <lgomez25@ucm.es>
2022-05-01 18:50:18 +0200

    Nuevo nivel de huida

---
+++ VerdantSorrow/VerdantSorrow/resources/Huida/Tilesets/objetosHuida.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="ObjetosHuida" tilewidth="666" tileheight="667" tilecount="25" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="Plataforma">
+  <image width="98" height="105" source="../../images/Nivel huida/medio plataforma.png"/>
+ </tile>
+ <tile id="1" type="Pincho">
+  <image width="321" height="175" source="../../images/Nivel huida/pincho chiquito 1 arr.png"/>
+ </tile>
+ <tile id="2" type="Pincho">
+  <image width="175" height="322" source="../../images/Nivel huida/pincho chiquito 1 dch.png"/>
+ </tile>
+ <tile id="3" type="Pincho">
+  <image width="174" height="321" source="../../images/Nivel huida/pincho chiquito 1 izq.png"/>
+ </tile>
+ <tile id="4" type="Pincho">
+  <image width="322" height="175" source="../../images/Nivel huida/pincho chiquito 1.png"/>
+ </tile>
+ <tile id="5" type="Pincho">
+  <image width="165" height="547" source="../../images/Nivel huida/pincho grande 1.png"/>
+ </tile>
+ <tile id="6" type="Pincho">
+  <image width="165" height="547" source="../../images/Nivel huida/pincho grande 2.png"/>
+ </tile>
+ <tile id="7" type="Pincho">
+  <image width="164" height="547" source="../../images/Nivel huida/pincho grande abjpng.png"/>
+ </tile>
+ <tile id="8" type="Pincho">
+  <image width="547" height="164" source="../../images/Nivel huida/pincho grande dch.png"/>
+ </tile>
+ <tile id="9" type="Pincho">
+  <image width="547" height="164" source="../../images/Nivel huida/pincho grande izq.png"/>
+ </tile>
+ <tile id="10" type="Plataforma">
+  <image width="106" height="105" source="../../images/Nivel huida/plataforma chikita.png"/>
+ </tile>
+ <tile id="11" type="seta">
+  <image width="414" height="238" source="../../images/Nivel huida/plataforma seta.png"/>
+ </tile>
+ <tile id="12" type="disparaMoscas">
+  <image width="666" height="568" source="../../images/Nivel huida/disparamoskaizquierda.png"/>
+ </tile>
+ <tile id="13" type="disparaMoscas">
+  <image width="568" height="667" source="../../images/Nivel huida/disparamoskas1sprite.png"/>
+ </tile>
+ <tile id="14" type="disparaMoscas">
+  <image width="568" height="666" source="../../images/Nivel huida/disparamoskasabajo.png"/>
+ </tile>
+ <tile id="15" type="disparaMoscas">
+  <image width="666" height="568" source="../../images/Nivel huida/disparamoskasderecha.png"/>
+ </tile>
+ <tile id="16" type="Plataforma">
+  <image width="105" height="103" source="../../images/Nivel huida/final plataforma abajo.png"/>
+ </tile>
+ <tile id="17" type="Plataforma">
+  <image width="105" height="103" source="../../images/Nivel huida/final plataforma arriba.png"/>
+ </tile>
+ <tile id="18" type="Plataforma">
+  <image width="103" height="105" source="../../images/Nivel huida/final plataforma.png"/>
+ </tile>
+ <tile id="19" type="Plataforma">
+  <image width="105" height="105" source="../../images/Nivel huida/inicio plataforma abajo.png"/>
+ </tile>
+ <tile id="20" type="Plataforma">
+  <image width="105" height="105" source="../../images/Nivel huida/inicio plataforma arriba.png"/>
+ </tile>
+ <tile id="21" type="Plataforma">
+  <image width="106" height="105" source="../../images/Nivel huida/inicio plataforma.png"/>
+ </tile>
+ <tile id="22" type="Plataforma">
+  <image width="98" height="105" source="../../images/Nivel huida/medio plataforma giro abajo.png"/>
+ </tile>
+ <tile id="23" type="Plataforma">
+  <image width="105" height="98" source="../../images/Nivel huida/medio plataforma giro dcha.png"/>
+ </tile>
+ <tile id="24" type="Plataforma">
+  <image width="105" height="98" source="../../images/Nivel huida/medio plataforma giro izq.png"/>
+ </tile>
+</tileset>
