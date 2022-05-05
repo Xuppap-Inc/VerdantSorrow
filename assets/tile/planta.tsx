addd20dc3a85bdcfca2bc4882f51e3bd983bbea9
Sebana02 <sebana@ucm.es>
2022-05-05 21:37:02 +0200

    cambios web

--- assets/tile/planta.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="planta" tilewidth="128" tileheight="128" tilecount="1" columns="1">
- <image source="planta.png" width="128" height="128"/>
-</tileset>
--- assets/tile/rama.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="rama" tilewidth="128" tileheight="128" tilecount="7" columns="1">
- <image source="rama.png" width="128" height="896"/>
-</tileset>
